37dea776da7378318deea03d358c29aaa19d774f
MikitaCat <stankevich.mikita21@gmail.com>
2024-04-09 23:09:26 +0200

    Vue.js lists rendering (options API)

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,8 +1,24 @@
 <template>
     <h3>History</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Cash <span>-$400</span><button class="delete-btn">x</button>
+        <li :class="transaction.amount < 0 ? 'minus' : 'plus'" v-for="transaction in transactions"
+            v-bind:key="transaction.id">
+            {{ transaction.text }} <span>{{ transaction.amount }}</span><button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            transactions: [
+                { id: 1, text: "Flower", amount: -19.99 },
+                { id: 2, text: "Salary", amount: 229.99 },
+                { id: 3, text: "Phone", amount: -10 },
+                { id: 4, text: "Guitar", amount: 150 },
+            ]
+        }
+    }
+}
+</script>
