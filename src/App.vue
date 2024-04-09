2b2e32eda751e38a6ff8b4a11be1b515f30fb6bd
MikitaCat <stankevich.mikita21@gmail.com>
2024-04-09 21:30:10 +0200

    Components list, how to import and components egistration

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  My App
+  <Header />
+  <div class="container">
+    <Balance />
+  </div>
 </template>
+
+<script>
+import Balance from './components/Balance.vue';
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header,
+    Balance
+  }
+}
+</script>
