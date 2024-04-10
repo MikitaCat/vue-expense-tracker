e84532b8058f69b4c835f049720b7b4df0cf6f91
MikitaCat <stankevich.mikita21@gmail.com>
2024-04-10 14:36:08 +0200

    App.vue, component imports without registration (Composition API, <script setup>

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,10 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import Balance from './components/Balance.vue';
 import Header from './components/Header.vue';
 import IncomeExpences from "./components/IncomeExpenses.vue"
 import TransactionList from "./components/TransactionList.vue"
 import AddTransaction from "./components/AddTransaction.vue"
-
-export default {
-  components: {
-    Header,
-    Balance,
-    IncomeExpences,
-    TransactionList,
-    AddTransaction
-  }
-}
 </script>
