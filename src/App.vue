e54a9b1c4ade51639f1092d6e03e9e831b6fb07f
MikitaCat <stankevich.mikita21@gmail.com>
2024-04-10 15:09:37 +0200

    Rendered list has been replaced to the App.vue component. Then this list has been put to TransactionList.vue as a prop and registered using defineProps function (yes, probably everytime we using props, we need to register it using defineProps

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <Balance />
     <IncomeExpences />
-    <TransactionList />
+    <TransactionList :transactions="transactions" />
     <AddTransaction />
   </div>
 </template>
@@ -14,4 +14,13 @@ import Header from './components/Header.vue';
 import IncomeExpences from "./components/IncomeExpenses.vue"
 import TransactionList from "./components/TransactionList.vue"
 import AddTransaction from "./components/AddTransaction.vue"
+
+import { ref } from 'vue';
+
+const transactions = ref([
+  { id: 1, text: "Flower", amount: -9.99 },
+  { id: 2, text: "Salary", amount: 229.99 },
+  { id: 3, text: "Phone", amount: -10 },
+  { id: 4, text: "Guitar", amount: 150 },
+])
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -9,10 +9,12 @@
 </template>
 
 <script setup>
-const transactions = [
-    { id: 1, text: "Flower", amount: -9.99 },
-    { id: 2, text: "Salary", amount: 229.99 },
-    { id: 3, text: "Phone", amount: -10 },
-    { id: 4, text: "Guitar", amount: 150 },
-]
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true
+    }
+})
 </script>
