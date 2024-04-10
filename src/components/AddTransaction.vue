293c891c979a5d1f27c0c33bc7d9050792e37cb6
MikitaCat <stankevich.mikita21@gmail.com>
2024-04-10 17:10:56 +0200

    v-model and submit event

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -3,19 +3,24 @@
     <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Text</label>
-            <input type="text" id="text" placeholder="Enter text..." />
+            <input v-model="text" type="text" id="text" placeholder="Enter text..." />
         </div>
         <div class="form-control">
             <label for="amount">Amount <br />
                 (negative - expense, positive - income)</label>
-            <input type="number" id="amount" placeholder="Enter amount..." />
+            <input v-model="amount" type="text" id="amount" placeholder="Enter amount..." />
         </div>
         <button class="btn">Add transaction</button>
     </form>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
+const text = ref("")
+const amount = ref("")
+
 const onSubmit = () => {
-    console.log("Submit")
+    console.log("Submit", text.value)
 }
 </script>
