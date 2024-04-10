f8df24c7b91ef026483d5aa127255ceae0defbb1
MikitaCat <stankevich.mikita21@gmail.com>
2024-04-10 17:24:35 +0200

    Vue-toastification installation and usage

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -16,11 +16,17 @@
 
 <script setup>
 import { ref } from 'vue';
+import { useToast } from "vue-toastification"
+
+const toast = useToast()
 
 const text = ref("")
 const amount = ref("")
 
 const onSubmit = () => {
+    if (!text.value) {
+        toast.error("Error")
+    }
     console.log("Submit", text.value)
 }
 </script>
